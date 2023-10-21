a8d40e796ca469c99c648f2b2733fc822bdf4146
ssammo <ssammo001@gmail.com>
2023-10-21 21:34:20 +0600

    mongoose updated
    let's check

--- app.js
+++ app.js
@@ -21,8 +21,13 @@ var app = express();
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect(process.env.MONGO_URL);
-
+  console.log(process.env.MONGO_URL);
+  try{
+    await mongoose.connect(process.env.MONGO_URL);
+  }catch (err) {
+    console.log(err);
+  }
+  
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
 
